f31e4052c139d65765dae5d43d8c4238fa14977d
carloschambidevutn <cchambi@frba.utn.edu.ar>
2026-04-08 03:22:19 +0000

    SERVER CONFIGURADO

--- server/src/utils.c
+++ server/src/utils.c
@@ -5,7 +5,7 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	//assert(!"no implementado!");
 
 	int socket_servidor;
 
@@ -19,10 +19,17 @@ int iniciar_servidor(void)
 	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
 	// Creamos el socket de escucha del servidor
-
+	int socket_escucha = socket(servinfo->ai_family,
+								servinfo->ai_socktype,
+								servinfo->ai_protocol);
 	// Asociamos el socket a un puerto
+	socket_servidor = setsockopt(socket_escucha, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
+	socket_servidor = bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
 
 	// Escuchamos las conexiones entrantes
+	socket_servidor = listen(socket_escucha, SOMAXCONN);
+
+	
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -33,10 +40,16 @@ int iniciar_servidor(void)
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	//assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
+
+	if(socket_cliente == -1){
+		log_error(logger, "Error al aceptar cliente");
+		return -1;
+	}
+
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
